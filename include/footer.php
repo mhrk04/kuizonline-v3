0603756cf2e74655b7598bb88642bc14b15e7ab8
mhrk04 <haziqrohaizan@gmail.com>
2021-10-15 07:06:44 +0800

    tambah footer

---
+++ include/footer.php
@@ -0,0 +1,19 @@
+<style>
+  .footer {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    background-color: red;
+    color: white;
+    text-align: center;
+  }
+</style>
+<div class="footer">
+  <center>
+    <font>
+      Copyright &copy; 2021
+      <?= $footer; ?>
+    </font>
+  </center>
+</div>
