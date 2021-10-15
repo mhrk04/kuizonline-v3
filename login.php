b0360a02bc38fd0e044669e525bf8ac597170ac1
mhrk04 <haziqrohaizan@gmail.com>
2021-10-15 09:34:15 +0800

    add html

--- login.php
+++ login.php
@@ -1,6 +1,5 @@
 <?php
 require "include/functions.php";
-include "include/header.php";
 ?>
 
 <!DOCTYPE html>
@@ -14,8 +13,8 @@ include "include/header.php";
   <style>
     table.login {
       width: 70%;
-      border: 1px solid black;
       align-items: center;
+      border: black solid 1px;
       margin-left: auto;
       margin-right: auto;
     }
@@ -23,13 +22,23 @@ include "include/header.php";
     td.login {
       text-align: center;
     }
+
+    .cen {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 200px;
+      border: 3px solid green;
+    }
   </style>
 </head>
 
 <body>
 
   <head>
-    <?php include "./include/menu1.php" ?>
+    <?php
+    include "include/header.php";
+    include "./include/menu1.php" ?>
   </head>
   <center>
     <h2>Log Masuk Pengguna</h2>
@@ -40,17 +49,33 @@ include "include/header.php";
         <tr>
           <td class="login">
             <label for="fname">
-              Nombor Kad Pengenalan :
+              No. Kad Pengenalan
             </label>
           </td>
           <td class="login">
-            <input id="fname" type="text" name="idpengguna">
+            <input id="fname" placeholder="No. Kad Pengenalan anda" type="text" name="idpengguna" maxlength='12' onkeypress='return event.charCode >= 8 && event.charCode <= 57' required autofocus>
           </td>
         </tr>
         <tr>
-          <td class="login"><label for="katalaluan">Katalalua :</label></td>
-          <td class="login"><input type="password" id="katalaluan" name="password"></td>
+          <td class="login">
+            <label for="katalaluan">Katalaluan</label>
+          </td>
+          <td class="login">
+            <input " type=" password" id="katalaluan" name="password" placeholder="Letakkan password anda" maxlength='4' onkeypress='return event.charCode >= 8 &&
+    event.charCode <= 57' required>
+          </td>
         </tr>
+        <tr>
+          <td colspan="2">
+            <center>
+              <button type="submit">Log Masuk</button>
+              <button type="reset">Reset</button>
+              <br>
+              <h5>*Jika belum mendaftar, <a href="./daftar_baru.php">Daftar di sini</a></h5>
+            </center>
+          </td>
+        </tr>
+
       </table>
     </form>
   </main>
