b9c80b79ccea195ffb65a8f4697860ae9dac20cb
mhrk04 <haziqrohaizan@gmail.com>
2021-11-11 04:48:20 +0800

    add multi select delete

--- senarai_pelajar.php
+++ senarai_pelajar.php
@@ -3,6 +3,16 @@ session_start();
 require "include/functions.php";
 sec();
 $pelajar = query("SELECT * FROM pengguna WHERE aras = 'PELAJAR' ORDER BY nama ASC");
+if (isset($_POST['buttonDelete'])) {
+  if (isset($_POST['username'])) {
+    foreach ($_POST['username'] as $pel) {
+      mysqli_query($conn, "DELETE FROM pengguna WHERE idpengguna = '$pel'");
+    }
+  }
+  echo "<script>
+          document.location.href = 'senarai_pelajar.php';
+          </script>";
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -34,31 +44,34 @@ $pelajar = query("SELECT * FROM pengguna WHERE aras = 'PELAJAR' ORDER BY nama AS
       <h2>Senarai Pelajar Berdaftar</h2>
     </center>
     <main>
-      <table align="center">
-        <tr>
-          <th width="5%">Bil.</th>
-          <th width="10%">ID Pelajar</th>
-          <th width="5%">Password</th>
-          <th width="50%">Nama Pelajar</th>
-          <th width="5%">Jantina</th>
-          <th width="5%">Tindakan</th>
-        </tr>
-        <?php $j = 1; ?>
-        <?php foreach ($pelajar as $pel) : ?>
-          <tr class="list">
-            <td><?= $j; ?></td>
-            <td><?= $pel["idpengguna"]; ?></td>
-            <td><?= $pel["password"]; ?></td>
-            <td><?= $pel["nama"]; ?></td>
-            <td><?= $pel["jantina"]; ?></td>
-            <td>
-              <!-- <a class="update" href="pelajar_update.php?id=">Ubah</a> -->
-              <a class="padam" href="hapus.php?id=<?= $pel['idpengguna']; ?>&table=pelajar&fill=IDPelajar" onclick="return confirm('Yakin hendak dipadam');">Padam</a>
-            </td>
+      <form action="" method="POST">
+        <center>
+          <input type="submit" name="buttonDelete" value="Delete" onclick="return confirm('Are you sure?')" />
+        </center>
+        <table align="center">
+          <tr>
+            <th width="1%"></th>
+            <th width="5%">Bil.</th>
+            <th width="10%">ID Pelajar</th>
+            <th width="5%">Password</th>
+            <th width="50%">Nama Pelajar</th>
+            <th width="5%">Jantina</th>
           </tr>
-          <?php $j++; ?>
-        <?php endforeach; ?>
-      </table>
+          <?php $j = 1; ?>
+          <?php foreach ($pelajar as $pel) : ?>
+            <tr class="list">
+              <td><input type="checkbox" value="<?= $pel['idpengguna']; ?>" name="username[]" id="padam"></td>
+              <td><?= $j; ?></td>
+              <td><?= $pel["idpengguna"]; ?></td>
+              <td><?= $pel["password"]; ?></td>
+              <td><?= $pel["nama"]; ?></td>
+              <td><?= $pel["jantina"]; ?></td>
+
+            </tr>
+            <?php $j++; ?>
+          <?php endforeach; ?>
+        </table>
+      </form>
     </main>
     <center>
       <font style="font-size: 14px;">*Senarai Tamat* <br>Jumlah Rekod : <?= $j - 1; ?></font>
